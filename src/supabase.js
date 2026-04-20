2600cae858b8b66b78aade0c6ba77154ffe39f9a
yihong Wu <1053512620@qq.com>
2026-04-20 11:28:11 +0800

    add github actions deploy

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://volngrfaxqunevopkkiu.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvbG5ncmZheHF1bmV2b3BraXUiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTY0NjgwOTk0MCwiZXhwIjoxOTYyMzg1OTQwfQ.SbT_X4SUdm2W4dKTAZZqBqLpBP7I_rKQeGL5_6u1zXU'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://volngrfaxqunevopkkiu.supabase.co'
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvbG5ncmZheHF1bmV2b3BraXUiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTY0NjgwOTk0MCwiZXhwIjoxOTYyMzg1OTQwfQ.SbT_X4SUdm2W4dKTAZZqBqLpBP7I_rKQeGL5_6u1zXU'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
